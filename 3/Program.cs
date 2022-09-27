181c3a1ee33721ab44d21569183b2e78a2a153d3
Nikoo90 <66570980+Nikoo90@users.noreply.github.com>
2022-09-27 22:53:24 +0300

    3

---
+++ 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+void printArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        if (arr.Length - 1 == i) Console.WriteLine($"{arr[i]}.");
+
+        else Console.Write($"{arr[i]}, ");
+
+
+    }
+}
+
+int[] createArr()
+{
+    int[] arr = new int[8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+printArr(createArr());
